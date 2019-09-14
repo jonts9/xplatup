b9966aa1b2eac6c7ee1f09a2c393fcf4127d7126
Jonatas Luiz <jonatasluiz9@gmail.com>
2019-09-14 10:20:12 -0300

    initial app, running

--- XplatCollect/XplatCollect/App.xaml.cs
+++ XplatCollect/XplatCollect/App.xaml.cs
@@ -1,31 +1,37 @@
-﻿using System;
+﻿using Prism;
+using Prism.DryIoc;
+using Prism.Ioc;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using XplatCollect.Views;
 
 namespace XplatCollect
 {
-    public partial class App : Application
+    public partial class App : PrismApplication
     {
-        public App()
+        public App() : this(null) { }
+        public App(IPlatformInitializer platformInitializer)
+            : base(platformInitializer)
+        {
+        }
+
+        public App(IPlatformInitializer platformInitializer, bool setFormsDependencyResolver)
+            : base(platformInitializer, true)
+        {
+        }
+
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            containerRegistry.RegisterForNavigation<NavigationPage>();
+            containerRegistry.RegisterForNavigation<HomePage>();
+        }
+
+        protected override async void OnInitialized()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
-        }
-
-        protected override void OnStart()
-        {
-            // Handle when your app starts
-        }
-
-        protected override void OnSleep()
-        {
-            // Handle when your app sleeps
-        }
-
-        protected override void OnResume()
-        {
-            // Handle when your app resumes
+            await NavigationService.NavigateAsync($"{nameof(NavigationPage)}/{nameof(HomePage)}");
         }
     }
 }
---
+++ XplatCollect/XplatCollect/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace XplatCollect.ViewModels
+{
+    public sealed class HomePageViewModel : BindableBase
+    {
+        private string title;
+        public string Title
+        {
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+    }
+}
