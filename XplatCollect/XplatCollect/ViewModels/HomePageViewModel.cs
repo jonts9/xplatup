d4e5434c438cbd593225f9513c8eabcf083e871e
Jonatas Luiz <jonatasluiz9@gmail.com>
2019-09-26 20:21:11 -0300

    tarefa de casa

--- XplatCollect/XplatCollect/ViewModels/HomePageViewModel.cs
+++ XplatCollect/XplatCollect/ViewModels/HomePageViewModel.cs
@@ -1,6 +1,9 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using Prism.Navigation;
 using Prism.Services;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace XplatCollect.ViewModels
 {
@@ -8,17 +11,27 @@ namespace XplatCollect.ViewModels
     {
         private string title;
 
+        private string collectionName;
+        public string CollectionName { get=>collectionName; set=>SetProperty(ref collectionName, value); }
+
         public HomePageViewModel(INavigationService navigationService
             , IPageDialogService pageDialogService)
             : base(navigationService, pageDialogService)
         {
-
+            ViewCollectionCommand = new DelegateCommand(async () => await ExecuteViewCollectionCommand());
         }
 
         public string Title
         {
             get => title;
             set => SetProperty(ref title, value);
+        }
+
+        public ICommand ViewCollectionCommand { get; set; }
+
+        private async Task ExecuteViewCollectionCommand()
+        {
+            await Task.CompletedTask;
         }
     }
 }
